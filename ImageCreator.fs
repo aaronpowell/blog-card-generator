9868de2cbef31436da62286463d1b9f6a11a8706
Aaron Powell <me@aaron-powell.com>
2020-01-03 15:23:45 +1100

    refactoring for readability

--- ImageCreator.fs
+++ ImageCreator.fs
@@ -9,59 +9,73 @@ open SixLabors.Primitives
 open System.IO
 open System
 
+let cornerRadius = 10.f
+let gutter = 20.f
+
 let getFontName =
     match Environment.OSVersion.Platform with
     | PlatformID.Unix -> "DejaVu Sans Mono"
     | _ -> "Consolas"
 
-let generateBox (colour: Color) (width: float32) (height: float32) (offsetX: float32) (offsetY: float32)
+
+let generateBox (colour: Color) (width: float32) (height: float32) (xOffset: float32) (yOffset: float32)
     (image: Image<Rgba32>) =
+    let xStart = gutter + cornerRadius
+    let yStart = gutter + cornerRadius
 
     // main box
-    image.Mutate
-        (fun ctx ->
+    image.Mutate(fun ctx ->
         ctx.Fill
             (GraphicsOptions(true), colour,
-             RectangularPolygon(40.f + offsetX, 40.f + offsetY, width - 70.f, height - 60.f)) |> ignore)
+             RectangularPolygon
+                 (xStart + xOffset, yStart + yOffset, width - xStart - gutter - cornerRadius, height - yStart - gutter))
+        |> ignore)
 
     // left gutter
-    image.Mutate
-        (fun ctx ->
-        ctx.Fill(GraphicsOptions(true), colour, RectangularPolygon(20.f + offsetX, 30.f + offsetY, 20.f, height - 50.f))
-        |> ignore)
-
-    // right gutter
-    image.Mutate
-        (fun ctx ->
+    image.Mutate(fun ctx ->
         ctx.Fill
             (GraphicsOptions(true), colour,
-             RectangularPolygon(width - 40.f + offsetX, 30.f + offsetY, 20.f, height - 50.f)) |> ignore)
+             RectangularPolygon(gutter + xOffset, yStart + yOffset, cornerRadius, height - yStart - gutter)) |> ignore)
 
-    // top gutter
-    image.Mutate
-        (fun ctx ->
-        ctx.Fill(GraphicsOptions(true), colour, RectangularPolygon(30.f + offsetX, 20.f + offsetY, width - 60.f, 20.f))
+    // right gutter
+    image.Mutate(fun ctx ->
+        ctx.Fill
+            (GraphicsOptions(true), colour,
+             RectangularPolygon(width - xStart + xOffset, yStart + yOffset, cornerRadius, height - yStart - gutter))
         |> ignore)
 
+    // top gutter
+    image.Mutate(fun ctx ->
+        ctx.Fill
+            (GraphicsOptions(true), colour,
+             RectangularPolygon
+                 (xStart + xOffset, gutter + yOffset, width - xStart - gutter - cornerRadius, cornerRadius)) |> ignore)
+
     // rounded corner - left top
-    image.Mutate
-        (fun ctx ->
-        ctx.Fill(GraphicsOptions(true), colour, EllipsePolygon(30.f + offsetX, 30.f + offsetY, 20.f, 20.f)) |> ignore)
+    image.Mutate(fun ctx ->
+        ctx.Fill
+            (GraphicsOptions(true), colour,
+             EllipsePolygon(xStart + xOffset, yStart + yOffset, cornerRadius * 2.f, cornerRadius * 2.f)) |> ignore)
 
     // rounded corner - right top
-    image.Mutate
-        (fun ctx ->
-        ctx.Fill(GraphicsOptions(true), colour, EllipsePolygon(width - 30.f + offsetX, 30.f + offsetY, 20.f, 20.f))
+    image.Mutate(fun ctx ->
+        ctx.Fill
+            (GraphicsOptions(true), colour,
+             EllipsePolygon(width - xStart + xOffset, yStart + yOffset, cornerRadius * 2.f, cornerRadius * 2.f))
         |> ignore)
 
     image
+
+#nowarn "77"
+let inline (!>) (x:^a) : ^b = ((^a or ^b) : (static member op_Explicit : ^a -> ^b) x) 
+#warn "77"
 
 let addText (text: string) (fontSize: float32) (xEnd: float32) (y: float32) (image: Image<Rgba32>) =
     let fam = getFontName |> SystemFonts.Find
     let font = Font(fam, fontSize)
 
     let pb = PathBuilder()
-    pb.SetOrigin(PointF(40.f, 0.f)) |> ignore
+    pb.SetOrigin(PointF(gutter * 2.f, 0.f)) |> ignore
     pb.AddLine(0.f, y, xEnd, y) |> ignore
     let path = pb.Build()
 
@@ -77,9 +91,7 @@ let addText (text: string) (fontSize: float32) (xEnd: float32) (y: float32) (ima
 
     let glyphs = TextBuilder.GenerateGlyphs(text, path, ro)
 
-    let gopts = TextGraphicsOptions.op_Explicit opts
-
-    image.Mutate(fun ctx -> ctx.Fill(gopts, Color.Black, glyphs) |> ignore)
+    image.Mutate(fun ctx -> ctx.Fill(!> opts, Color.Black, glyphs) |> ignore)
 
     image
 
@@ -87,17 +99,18 @@ let makeImage width height title author (date: DateTimeOffset) tags =
     let image = new Image<Rgba32>(int width, int height)
     image.Mutate(fun ctx -> ctx.Fill(Color.FromHex "02bdd5") |> ignore)
 
-    generateBox (Rgba32.op_Implicit <| Rgba32.FromHex "333") width height 5.f 5.f image
-    |> generateBox (Rgba32.op_Implicit Rgba32.White) width height 0.f 0.f
-    |> addText title 30.f (width - 60.f) (height / 2.f)
-    |> addText (sprintf "%s | %s" author (date.ToString "MMMM dd, yyyy")) 20.f (width - 60.f) (height / 2.f + 40.f)
+    let textX = width - (gutter + cornerRadius) * 2.f
+    let textY = height / 2.f
+
+    generateBox (Color.FromHex "333") width height 5.f 5.f image
+    |> generateBox Color.White width height 0.f 0.f
+    |> addText title 30.f textX textY
+    |> addText (sprintf "%s | %s" author (date.ToString "MMMM dd, yyyy")) 20.f textX (textY + 40.f)
     |> addText
         (tags
          |> Array.map (fun t -> sprintf "#%s" t)
          |> Array.toSeq
-         |> String.concat " ") 15.f (width - 60.f) (height / 2.f + 70.f)
-    |> ignore
-    image
+         |> String.concat " ") 15.f textX (textY + 70.f)
 
 let imageToStream (image: Image<Rgba32>) =
     let ms = new MemoryStream()
